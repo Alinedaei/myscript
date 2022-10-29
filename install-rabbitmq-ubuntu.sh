e7fc2553b71865176d2a4d62c9346041d311c24d
Alinedaei <86824959+Alinedaei@users.noreply.github.com>
2022-10-29 16:31:02 +0330

    rabbitmq
    1

---
+++ install-rabbitmq-ubuntu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt update
+sudo apt-get install wget apt-transport-https -y
+wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
+echo "deb https://dl.bintray.com/rabbitmq-erlang/debian focal erlang-22.x" | sudo tee /etc/apt/sources.list.d/rabbitmq.list
+sudo apt-get install rabbitmq-server -y --fix-missing
+sudo rabbitmqctl add_user admin R@bbitmQ123
+echo "user:admin,passwd:R@bbitmQ"
+sudo systemctl status rabbitmq-server
+systemctl is-enabled rabbitmq-server.service
+sudo systemctl enable rabbitmq-server
+sudo rabbitmq-plugins enable rabbitmq_management
+ sudo ss -tunelp | grep 15672
+ sudo ufw allow proto tcp from any to any port 5672,15672
+ hostname -I
+ echo"http://yourip:15672"
+ 
